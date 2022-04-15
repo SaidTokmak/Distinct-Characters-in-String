c18c932a6f210931481d6341eb4180690afff613
Said Tokmak <tokmaksaid@gmail.com>
2022-04-16 01:32:37 +0300

    improve code quality

--- Distinct.java
+++ Distinct.java
@@ -1,13 +1,20 @@
+/*
+* Copyright SaidTokmak 2022
+*/
+
 import java.util.*;
 
 public class Distinct {
 
+	/* if want to see debug, remove comment and add static method
 	public static void main(String[] args) {
-		distinctCharacterInString("Hello World");
-	}
+		 String distinctStr = getStringFromCharacterList(distinctCharacterInString("Hello World"));
+		 System.out.println(distinctStr); if want to see debug, remove comment
+	}*/
 
-	public static List<Character> distinctCharacterInString(String coins) {
+	public List<Character> distinctCharacterInString(String coins) {
 	    List<Character> ch = new ArrayList<>(); //list of distinct character in string value
+	    
 	    for (int i=0;i<coins.length();i++) {
 	        boolean has=false;  //firstly for each character represent not exist
 	        for(int j=0;j<ch.size();j++){
@@ -21,7 +28,17 @@ public class Distinct {
 	            ch.add(coins.charAt(i));
 	        }
 	    }
-	    //System.out.println(ch); if want to see debug, remove comment
+
 	    return ch;
+	}
+
+	public static String getStringFromCharacterList(List<Character> list) {    
+	    StringBuilder builder = new StringBuilder(list.size());
+	    
+	    for(Character ch: list) {
+	        builder.append(ch);
+	    }
+
+	    return builder.toString();
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Distinct-Characters-in-String
+
+This code removing same character and return distinct character list.
